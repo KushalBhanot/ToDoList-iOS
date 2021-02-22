4747a985eafb0fbe284d459d8b0ef76164221729
Kushal Bhanot <developerkushalb@gmail.com>
2021-02-22 13:08:17 +0530

    Implement the core ToDo functionality
    - Made a TableView
    - Added alert flow for adding new tasks
    - Handed memory leak by [weak self]
    - Adding new tasks take place in the main thread(DispatchQueue.main.async)

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,68 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by kushal on 22/02/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    private let table: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "toDoCell")
+        return table
+    }()
+    
+    var tasks = [String]()
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        title = "To Do List"
+        
+        view.addSubview(table)
+        table.dataSource = self
+    }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        table.frame = view.bounds
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector(didTapAdd))
+    }
+    
+    @objc func didTapAdd() {
+        let alert = UIAlertController(title: "New Task", message: "Enter new task to add it in the list", preferredStyle: .alert)
+        
+        alert.addTextField { field in
+            field.placeholder = "Enter task..."
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "Done", style: .default, handler: { [weak self] _ in
+            if let field = alert.textFields?.first {
+                if let text = field.text, !text.isEmpty {
+                    DispatchQueue.main.async {
+                        self?.tasks.append(text)
+                        self?.table.reloadData()
+                    }
+                }
+            }
+        }))
+        
+        present(alert, animated: true)
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = table.dequeueReusableCell(withIdentifier: "toDoCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+}
+
